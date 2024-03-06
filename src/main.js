c342af5f73c555c0b407a23dcd399bdb2a0b9f39
Andrea Avanzi <andrea.avanzi@ctinnovation.it>
2024-03-06 19:58:25 +0100

    fix

--- src/main.js
+++ src/main.js
@@ -166,7 +166,7 @@ function createCard({ name, status, owner, repo, eventName, ref, actor, workflow
           },
           {
             decoratedText: {
-              icon: { iconUrl: 'https://raw.githubusercontent.com/ctinnovation/google-chat-github-action/main/assets/donload.png' },
+              icon: { iconUrl: 'https://raw.githubusercontent.com/ctinnovation/google-chat-github-action/main/assets/download.png' },
               topLabel: 'Download',
               text: artifactUrl,
               button: { text: 'Download', onClick: { openLink: { url: artifactUrl } } }
